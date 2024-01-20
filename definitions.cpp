36608ab3fa545bf2f24378744057674634ad8121
Terex RH400 Mining Excavator <71463391+Terex-RH00-Mining-Excavator@users.noreply.github.com>
2024-01-19 16:46:10 -0800

    Create definitions.cpp

---
+++ definitions.cpp
@@ -0,0 +1,17 @@
+#include "declarations.hpp"
+
+int add(int a, int b){
+    return a + b;
+}
+
+int subtract(int a, int b){
+    return a - b;
+}
+
+int multiply(int a, int b){
+    return a * b;
+}
+
+float divide(int a, int b){
+    return (float) a / b;
+}
